a694724938f90565e7f8a81862541e6014c5ee54
ynb4gang <129555715+ynb4gang@users.noreply.github.com>
2023-11-24 15:41:40 +0300

    update

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -5,41 +5,43 @@ import hexlet.code.Engine;
 public class Prime {
     private static final int DIVISOR_6 = 3;
     private static final int DIVISOR_5 = 5;
+
     public static void play() {
         Engine.getRulesPrime();
-        String[][] questionsAndAnswers = questionsAndAnswers();
+        String[][] questionsAndAnswers = generateQuestionsAndAnswers();
         Engine.run(questionsAndAnswers);
     }
-    private static String[][] questionsAndAnswers() {
+
+    private static String[][] generateQuestionsAndAnswers() {
         String[][] questionsAndAnswers = new String[Engine.MAGIC_NUMBER_3][2];
         for (int iterationLimit = 0; iterationLimit < Engine.MAGIC_NUMBER_3; iterationLimit++) {
             int number = Engine.getRandomNumber();
-            String numberString = String.valueOf(Engine.getRandomNumber());
+            String numberString = String.valueOf(number);
             questionsAndAnswers[iterationLimit][0] = numberString;
-            questionsAndAnswers[iterationLimit][1] = isPrime(number);
+            questionsAndAnswers[iterationLimit][1] = isPrime(number) ? "yes" : "no";
         }
         return questionsAndAnswers;
     }
-    public static String isPrime(int number) {
-        if (number <= 1) {
-            return "no";
-        }
-        if (number == 2 || number == Engine.MAGIC_NUMBER_3) {
-            return "yes";
-        }
-        if (number % 2 == 0 || number % Engine.MAGIC_NUMBER_3 == 0) {
-            return "no";
-        }
-        int i = DIVISOR_5;
-        int w = 2;
-        while (i * i <= number) {
-            if (number % i == 0) {
-                return "no";
-            }
 
-            i += w;
-            w = DIVISOR_6 - w;
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
         }
-        return "yes";
+        if (number == 2 || number == DIVISOR_6) {
+            return true;
+        }
+        if (number % 2 == 0 || number % DIVISOR_6 == 0) {
+            return false;
+        }
+        int divisor = DIVISOR_5;
+        int step = 2;
+        while (divisor * divisor <= number) {
+            if (number % divisor == 0) {
+                return false;
+            }
+            divisor += step;
+            step = DIVISOR_6 - step;
+        }
+        return true;
     }
 }
